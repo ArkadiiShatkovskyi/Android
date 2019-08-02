7421905a000e933d7d319e49d0bd6c06eccae4d8
Arkadii <arkadii.shatkovskyi@gmail.com>
2019-08-02 23:36:39 +0200

    DateTime pickers were added

--- Flutter_Projects/job_app/lib/pageElements/BottomSheetWidget.dart
+++ Flutter_Projects/job_app/lib/pageElements/BottomSheetWidget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ant_icons/ant_icons.dart';
 import 'package:job_app/items/StyleSettings.dart';
+import 'package:intl/intl.dart';
 
 class BottomSheetWidget extends StatefulWidget{
   @override
@@ -8,6 +9,12 @@ class BottomSheetWidget extends StatefulWidget{
 }
 
 class _BottomSheetState extends State<BottomSheetWidget>{
+
+  TimeOfDay _strTime = TimeOfDay.now();
+  TimeOfDay _endTime = TimeOfDay.now();
+  DateTime _date =  DateTime.now();
+  String _rate;
+  String _workTime;
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +24,7 @@ class _BottomSheetState extends State<BottomSheetWidget>{
         margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
         decoration: BoxDecoration(
             color: Colors.grey[200],
-            borderRadius: BorderRadius.circular(10)
+            borderRadius: BorderRadius.circular(30)
         ),
         child: _createBody(),
     );
@@ -40,7 +47,9 @@ class _BottomSheetState extends State<BottomSheetWidget>{
           icon: Icon(AntIcons.clock_circle_outline),
           iconSize: 40,
           color: styleColor,
-          onPressed: (){},
+          onPressed: (){
+            _showStartTimePicker(context);
+          },
         ),
         const Text(""),
         const Text(""),
@@ -50,17 +59,21 @@ class _BottomSheetState extends State<BottomSheetWidget>{
           icon: Icon(AntIcons.clock_circle_outline),
           iconSize: 40,
           color: styleColor,
-          onPressed: (){},
+          onPressed: (){
+            _showEndTimePicker(context);
+          },
         ),
         const Text(""),
         const Text(""),
-        const Center(child:Text("Choose end time", style: TextStyle(fontSize: 16))),
+        const Center(child:Text("Choose date", style: TextStyle(fontSize: 16))),
         const Text(""),
         IconButton(
           icon: Icon(AntIcons.calendar_outline),
           iconSize: 40,
           color: styleColor,
-          onPressed: (){},
+          onPressed: (){
+            _showDatePicker(context);
+          },
         ),
         const Text(""),
         const Text(""),
@@ -87,5 +100,43 @@ class _BottomSheetState extends State<BottomSheetWidget>{
         const Text(""),
       ],
     );
+  }
+
+  Future<Null> _showDatePicker(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        initialDatePickerMode: DatePickerMode.day,
+        context: context,
+        initialDate: _date,
+        firstDate: DateTime(2015, 8),
+        lastDate: DateTime(2050));
+    if (picked != null && picked != _date)
+      setState(() {
+        _date = picked;
+//        print("date: " + _date.toString().substring(5, 10));
+      });
+  }
+
+  Future<Null> _showStartTimePicker(BuildContext context) async {
+    final TimeOfDay picked = await showTimePicker(
+      context: context,
+      initialTime: _strTime,
+    );
+    if (picked != null && picked != _strTime)
+      setState(() {
+        _strTime = picked;
+//        print("str time: " + _strTime.toString().substring(10, 15));
+      });
+  }
+
+  Future<Null> _showEndTimePicker(BuildContext context) async {
+    final TimeOfDay picked = await showTimePicker(
+      context: context,
+      initialTime: _endTime,
+    );
+    if (picked != null && picked != _endTime)
+      setState(() {
+        _endTime = picked;
+//        print("end time: " + _endTime.toString().substring(10, 15));
+      });
   }
 }
