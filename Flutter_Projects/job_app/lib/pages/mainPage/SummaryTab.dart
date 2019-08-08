a361e9b0b5169d39d275ea4e4367a40d5af66f53
Arkadii <arkadii.shatkovskyi@gmail.com>
2019-08-09 00:07:49 +0200

    New option UI was created

--- Flutter_Projects/job_app/lib/pages/mainPage/SummaryTab.dart
+++ Flutter_Projects/job_app/lib/pages/mainPage/SummaryTab.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:job_app/pages/authorizationPage/Authorization.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:job_app/items/StyleSettings.dart';
 
 class SummaryTab extends StatefulWidget{
 
@@ -11,6 +12,8 @@ class SummaryTab extends StatefulWidget{
 class _SummaryTabState extends State<SummaryTab>{
   double _workedTime = 0.0;
   double _salary = 0.0;
+  double _salaryPerMonth = 0;
+  double _workTimePerMonth = 0;
   String _user;
   Authorization _authorization = new Authorization();
 
@@ -50,15 +53,40 @@ class _SummaryTabState extends State<SummaryTab>{
           children: <Widget>[
             Divider(height: 25, color: Colors.transparent,),
             const Center(child:Text("Summary", style: TextStyle(fontSize: 22),)),
-            Divider(height: 25,),
+            Divider(height: 25, color: styleColor,),
+            const Center(child: Text("Per month", style: TextStyle(fontSize: 18),)),
             Container(
                 child: Row(
                   children: <Widget>[
                     Container(
-                        child: const Text("Time worked: ", style: TextStyle(fontSize: 18),),
-                        width: 150,
+                      child: const Text("Time: ", style: TextStyle(fontSize: 16),),
+                      width: 170,
                     ),
-                    Text(_workedTime.toString(), style: TextStyle(fontSize: 18),)
+                    Text(_workTimePerMonth.toString(), style: TextStyle(fontSize: 16),)
+                  ],
+                )
+            ),
+            Container(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      child: const Text("Salary: ", style: TextStyle(fontSize: 16),),
+                      width: 170,
+                    ),
+                    Text(_salaryPerMonth.toString(), style: TextStyle(fontSize: 16),)
+                  ],
+                )
+            ),
+            Divider(height: 25, color: Colors.transparent,),
+            const Center(child: Text("Total", style: TextStyle(fontSize: 18),),),
+            Container(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                        child: const Text("Time worked: ", style: TextStyle(fontSize: 16),),
+                        width: 170,
+                    ),
+                    Text(_workedTime.toString(), style: TextStyle(fontSize: 16),)
                   ],
             )
             ),
@@ -66,10 +94,10 @@ class _SummaryTabState extends State<SummaryTab>{
                 child: Row(
                   children: <Widget>[
                     Container(
-                      child: const Text("Salary: ", style: TextStyle(fontSize: 18),),
-                      width: 150,
+                      child: const Text("Salary: ", style: TextStyle(fontSize: 16),),
+                      width: 170,
                     ),
-                    Text(_salary.toString(), style: TextStyle(fontSize: 18),)
+                    Text(_salary.toString(), style: TextStyle(fontSize: 16),)
               ],
             )
             ),
