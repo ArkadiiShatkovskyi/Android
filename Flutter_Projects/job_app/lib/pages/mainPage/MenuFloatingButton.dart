2eca2dd66e590a77726ddffe049cf8a0d5875e0a
Arkadii <arkadii.shatkovskyi@gmail.com>
2019-08-22 14:58:41 +0200

    Created custom floating button

---
+++ Flutter_Projects/job_app/lib/pages/mainPage/MenuFloatingButton.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:job_app/items/StyleSettings.dart';
+
+class FloatingButtonMenu extends StatefulWidget {
+  @override
+  _FloatingButtonMenuState createState() => _FloatingButtonMenuState();
+}
+
+class _FloatingButtonMenuState extends State<FloatingButtonMenu>
+    with SingleTickerProviderStateMixin {
+  bool isOpened = false;
+  AnimationController _animationController;
+  Animation<Color> _animateColor;
+  Animation<double> _animateIcon;
+  Curve _curve = Curves.easeOut;
+
+  @override
+  initState() {
+    _animationController =
+    AnimationController(vsync: this, duration: Duration(milliseconds: 300))
+      ..addListener(() {
+        setState(() {});
+      });
+    _animateIcon =
+        Tween<double>(begin: 0.0, end: 1.0).animate(_animationController);
+    _animateColor = ColorTween(
+      begin: styleColor,
+      end: Colors.red,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(
+        0.00,
+        1.00,
+        curve: _curve,
+      ),
+    ));
+    super.initState();
+  }
+
+  @override
+  dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  animate() {
+    if (!isOpened) {
+      _animationController.forward();
+    } else {
+      _animationController.reverse();
+    }
+    isOpened = !isOpened;
+  }
+
+  Widget toggle() {
+    return FloatingActionButton(
+      backgroundColor: _animateColor.value,
+      onPressed: animate,
+      tooltip: 'Toggle',
+      child: AnimatedIcon(
+        icon: AnimatedIcons.menu_close,
+        progress: _animateIcon,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return toggle();
+  }
+}
